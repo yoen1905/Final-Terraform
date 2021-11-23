7fd613b53ab46bbeb27c559ecb9c6567583681a8
yoen1905 <yn1905@naver.com>
2021-11-23 11:22:47 +0900

    second

--- 06_instance.tf
+++ 06_instance.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "mgmt" {
 
 
 
-resource "aws_instance" "web_apache" {
+resource "aws_instance" "web" {
   count                  = length(var.zone)
   ami                    = var.ami
   instance_type          = "t2.micro"
@@ -42,20 +42,20 @@ resource "aws_instance" "web_apache" {
     }
 }
 
-resource "aws_instance" "web_nginx" {
-  count                  = length(var.zone)
-  ami                    = var.ami
-  instance_type          = "t2.micro"
-  key_name               = var.key_pair
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  availability_zone      = "${var.region}${var.zone[count.index]}"
-  private_ip             = null
-  subnet_id              = aws_subnet.web_subnet[count.index].id
-  tags = {
-    Name                 = "Final-web-${var.zone[count.index]}"
-    Type                 = "nginx-${var.zone[count.index]}"
-    }
-}
+# resource "aws_instance" "web_nginx" {
+#   count                  = length(var.zone)
+#   ami                    = var.ami
+#   instance_type          = "t2.micro"
+#   key_name               = var.key_pair
+#   vpc_security_group_ids = [aws_security_group.web_sg.id]
+#   availability_zone      = "${var.region}${var.zone[count.index]}"
+#   private_ip             = null
+#   subnet_id              = aws_subnet.web_subnet[count.index].id
+#   tags = {
+#     Name                 = "Final-web-${var.zone[count.index]}"
+#     Type                 = "nginx-${var.zone[count.index]}"
+#     }
+# }
 
 
 resource "aws_instance" "was" {
--- 07_load_balancer.tf
+++ 07_load_balancer.tf
@@ -1,104 +1,104 @@
-# ########### ALB ###########
+########### ALB ###########
 
-# resource "aws_lb" "alb" {
-#   name               = "Final-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.alb_sg.id]
-#   subnets            = aws_subnet.pub_subnet.*.id
-#   # access_logs {
-#   #   bucket  = aws_s3_bucket.lb_log_s3.bucket
-#   #   prefix  = "test-lb"
-#   #   enabled = true
-#   # }
+resource "aws_lb" "alb" {
+  name               = "Final-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.pub_subnet.*.id
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_log_s3.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
 
-#   depends_on = [
-#     aws_instance.web
-#   ]
+  depends_on = [
+    aws_instance.web
+  ]
 
-#  tags = {
-#     Name = "Final-alb"
-#   }
+ tags = {
+    Name = "Final-alb"
+  }
 
-# }
-
-
-# # alb tg
-
-# resource "aws_lb_target_group" "alb_tg" {
-#   name        = "Final-alb-tg"
-#   port        = 80
-#   protocol    = "HTTP"
-#   vpc_id      = aws_vpc.my_vpc.id
+}
 
 
-#   health_check {
-#     enabled             = true
-#     healthy_threshold   = 3
-#     interval            = 5
-#     matcher             = "200" 
-#     path                = "/"
-#     port                = "traffic-port"
-#     protocol            = "HTTP"
-#     timeout             = 2
-#     unhealthy_threshold = 2
-#   }
-# }
+# alb tg
+
+resource "aws_lb_target_group" "alb_tg" {
+  name        = "Final-alb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.my_vpc.id
 
 
-# # alb listener
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 5
+    matcher             = "200" 
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 2
+    unhealthy_threshold = 2
+  }
+}
 
-# resource "aws_lb_listener" "alb_listener" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb_tg.arn
-#   }
-# }
+# alb listener
 
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
 
 
 
 
 
 
-########### NLB ###########
-
-# resource "aws_lb" "nlb" {
-#   name               = "Final-nlb"
-#   internal           = true
-#   load_balancer_type = "network"
-#   subnets            = aws_subnet.web_subnet.*.id
-#   depends_on = [
-#     aws_instance.was
-#   ]
-# }
 
 
-# # nlb tg
+########## NLB ###########
 
-# resource "aws_lb_target_group" "nlb_tg" {
-#   name = "Final-nlb-tg"
-#   port = 8080
-#   protocol = "TCP"
-#   vpc_id = aws_vpc.my_vpc.id
-# }
+resource "aws_lb" "nlb" {
+  name               = "Final-nlb"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = aws_subnet.web_subnet.*.id
+  depends_on = [
+    aws_instance.was
+  ]
+}
 
 
-# # nlb listener
+# nlb tg
 
-# resource "aws_lb_listener" "nlb_listener" {
-#   load_balancer_arn = aws_lb.nlb.arn
-#   port = "8080"
-#   protocol = "TCP"
+resource "aws_lb_target_group" "nlb_tg" {
+  name = "Final-nlb-tg"
+  port = 8080
+  protocol = "TCP"
+  vpc_id = aws_vpc.my_vpc.id
+}
 
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.nlb_tg.arn
-#   }
+
+# nlb listener
+
+resource "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port = "8080"
+  protocol = "TCP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.nlb_tg.arn
+  }
   
-# }
+}
